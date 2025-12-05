2627cd00d00b4ff058b3724a64046c5f079a41b7
cici-uu7 <ld17662053425@gmail.com>
2025-12-05 10:59:07 +0800

    updated main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     parser.add_argument('--pretrained_model', type=str, default=None)
     parser.add_argument('--dataset', type=str, default='credit')
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
-    parser.add_argument('--data_path', type=str, default='./dataset/creditcard_ts.csv')
+    parser.add_argument('--data_path', type=str, default='./dataset')
     parser.add_argument('--model_save_path', type=str, default='checkpoints')
     parser.add_argument('--anormly_ratio', type=float, default=4.00)
 
